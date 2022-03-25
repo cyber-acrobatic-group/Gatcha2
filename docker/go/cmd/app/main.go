5dd627d5a69a6862e6a427e7636e8bf64871f605
koshiterada <koshi.terada@o-kun.com>
2022-03-25 23:58:37 +0900

    second commit

---
+++ docker/go/cmd/app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cyber-acrobatic-group/Gatcha2/docker/go/cmd/app/router"
+)
+
+func main() {
+	fmt.Println("main")
+	router.Setting()
+
+}
---
+++ docker/go/cmd/app/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func Setting() {
+	fmt.Println("router")
+	router := gin.Default()
+	router.POST("/photo", func(context *gin.Context) {
+		fmt.Println("hogehoge")
+
+	})
+	router.Run(":3000")
+}
